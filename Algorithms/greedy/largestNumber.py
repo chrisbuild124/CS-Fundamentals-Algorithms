5b3cba61cbe91e9076a1b2a1d054596d231e4c69
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-30 17:50:37 -0400

    Update largestNumber.py

--- Algorithms/greedy/largestNumber.py
+++ Algorithms/greedy/largestNumber.py
@@ -11,3 +11,19 @@
         if num_strings[0] == "0":
             return "0"
         return "".join(num_strings)
+
+# or 
+from functools import cmp_to_key
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i, num in enumerate(nums):
+            nums[i] = str(num)
+
+        def compare(n1, n2):
+            if n1 + n2 > n2 + n1:
+                return -1
+            else:
+                return 1
+        nums = sorted(nums, key=cmp_to_key(compare))
+        return str(int("".join(nums)))
